c56ab38127b636137131777657182f5738f12315
dough <dough@Dough-PC.localdomain>
2023-02-25 16:12:50 -0600

    sign up done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -26,6 +26,30 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+export const restoreUser = () => async dispatch => {
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
@@ -42,13 +66,6 @@ const sessionReducer = (state = initialState, action) => {
     default:
       return state;
   }
-};
-
-export const restoreUser = () => async dispatch => {
-  const response = await csrfFetch('/api/session');
-  const data = await response.json();
-  dispatch(setUser(data.user));
-  return response;
 };
 
 export default sessionReducer;
