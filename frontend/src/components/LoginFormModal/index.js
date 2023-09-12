fc06e1a1928036d7ee107d9173f0896d20c7e7a0
dough <dough@Dough-PC.localdomain>
2023-09-11 20:32:31 -0500

    css

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -29,35 +29,40 @@ function LoginFormModal() {
 
   return (
     <div className='form-container'>
-      <div className="login-header">
-        
-      </div>
+      <div className="form-sub-container">
+        <div className="login-header">
+          <img className="login-icon" src="https://res.cloudinary.com/dcbexnl8j/image/upload/v1693948323/meetup%20shit/meetup-icon_n56qwx.png" />
+          <h1>Log in</h1>
+          <p>Not a member yet?</p>
+        </div>
 
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-        </ul>
-        <label className='headers'>
-          Email
-          <input
-            type="text"
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
-            required
-          />
-        </label>
-        <label className='headers'>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
-        <button type='submit' onClick={handleDemo}>Demo User</button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <ul>
+            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+          </ul>
+          <label className='login-labels'>
+            Email
+            <input className="login-inputs"
+              type="text"
+              value={credential}
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
+          </label>
+          <label className='login-labels'>
+            Password
+            <input className="login-inputs"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          <button type="submit">Log In</button>
+          <button type='submit' onClick={handleDemo}>Demo User</button>
+        </form>
+
+      </div>
     </div>
   );
 }
